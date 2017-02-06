56f103c98634bca100878f87f49b30fc48c528a4
Kevin Sanches <kevinsanches@gmail.com>
2017-02-06 07:23:33 -0200

    Changes related to hardware

--- PWMControl.cpp
+++ PWMControl.cpp
@@ -9,8 +9,16 @@ void setup() {
 
 void departure(){
 	int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
-	for (int x=0; x<1024; x++){
-		if ((millis()-time)>29){ //Every time that 29 miliseconds has passed:
+	for (int x=0; x<512; x++){
+		if ((millis()-time)>5){ //Every time that 29 miliseconds has passed:
+			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
+			PWMControl = ((control*255)/5); //Variable which will return the PWM value
+			analogWrite(baseTBJ, PWMControl);
+			time = millis();
+		}
+	}
+	for (int x=512; x<1024; x++){
+		if ((millis()-time)>19){ //Every time that 29 miliseconds has passed:
 			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
 			PWMControl = ((control*255)/5); //Variable which will return the PWM value
 			analogWrite(baseTBJ, PWMControl);
@@ -20,12 +28,20 @@ void departure(){
 }
 
 void stop(){
-	int time = millis();
-	for (int x=1024; x>0; x--){
-		if ((millis()-time)>29){
-			control = ((x*5)/1024);
-			PWMControl = ((control*255)/5);
-			analogWrite(PWMPin, PWMControl);
+	int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
+	for (int x=1024; x>512; x--){
+		if ((millis()-time)>19){ //Every time that 29 miliseconds has passed:
+			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
+			PWMControl = ((control*255)/5); //Variable which will return the PWM value
+			analogWrite(baseTBJ, PWMControl);
+			time = millis();
+		}
+	}
+	for (int x=512; x>0; x--){
+		if ((millis()-time)>5){ //Every time that 29 miliseconds has passed:
+			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
+			PWMControl = ((control*255)/5); //Variable which will return the PWM value
+			analogWrite(baseTBJ, PWMControl);
 			time = millis();
 		}
 	}
