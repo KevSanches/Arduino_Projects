ac6450ea13c90952110ac57d1f70de3357ebd026
Kevin Sanches <kevinsanches@gmail.com>
2017-02-06 08:30:25 -0200

    Changes do logic

--- PWMControl.cpp
+++ PWMControl.cpp
@@ -7,19 +7,23 @@ void setup() {
 }
 
 void departure(){
+	int y = 20;
 	for (int x=200; x<256; x++){
 		int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
-		if ((millis()-time)>20){ //Every time that 5 miliseconds has passed:
+		if ((millis()-time)>y){ //Every time that 5 miliseconds has passed:
 			analogWrite(baseTBJ, x);
+			y++;
 		}
 	}
 }
 
 void stop(){
+	int y = 20;
 	for (int x=255; x>200; x--){
 		int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
-		if ((millis()-time)>20){ //Every time that 29 miliseconds has passed:
+		if ((millis()-time)>y){ //Every time that 29 miliseconds has passed:
 			analogWrite(baseTBJ, x);
+			y++;
 		}
 	}
 }
