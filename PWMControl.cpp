f4b9857995470caa4a349e3fbee518288407d07b
Kevin Sanches <kevinsanches@gmail.com>
2017-02-06 07:53:42 -0200

    Changes on the logic

--- PWMControl.cpp
+++ PWMControl.cpp
@@ -1,6 +1,5 @@
 int baseTBJ = 7;
-int control = 0;
-int PWMControl = 0;
+int command = 0;
 
 void setup() {
 	pinMode(baseTBJ, OUTPUT);
@@ -8,53 +7,34 @@ void setup() {
 }
 
 void departure(){
-	int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
-	for (int x=0; x<512; x++){
-		if ((millis()-time)>5){ //Every time that 29 miliseconds has passed:
-			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
-			PWMControl = ((control*255)/5); //Variable which will return the PWM value
-			analogWrite(baseTBJ, PWMControl);
-			time = millis();
-		}
-	}
-	for (int x=512; x<1024; x++){
-		if ((millis()-time)>19){ //Every time that 29 miliseconds has passed:
-			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
-			PWMControl = ((control*255)/5); //Variable which will return the PWM value
-			analogWrite(baseTBJ, PWMControl);
-			time = millis();
+	for (int x=200; x<256; x++){
+		int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
+		if ((millis()-time)>20){ //Every time that 5 miliseconds has passed:
+			analogWrite(baseTBJ, x);
 		}
 	}
 }
 
 void stop(){
-	int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
-	for (int x=1024; x>512; x--){
-		if ((millis()-time)>19){ //Every time that 29 miliseconds has passed:
-			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
-			PWMControl = ((control*255)/5); //Variable which will return the PWM value
-			analogWrite(baseTBJ, PWMControl);
-			time = millis();
-		}
-	}
-	for (int x=512; x>0; x--){
-		if ((millis()-time)>5){ //Every time that 29 miliseconds has passed:
-			control = ((x*5)/1024);//Creates a control, so that the departure can be gradative
-			PWMControl = ((control*255)/5); //Variable which will return the PWM value
-			analogWrite(baseTBJ, PWMControl);
-			time = millis();
+	for (int x=255; x>200; x--){
+		int time = millis(); //Assign the "Time" variable with the millis function, which returns the time that has passed since the Arduino is turned on
+		if ((millis()-time)>20){ //Every time that 29 miliseconds has passed:
+			analogWrite(baseTBJ, x);
 		}
 	}
 }
 
 void loop(){ 
-	if (Serial.read() == "Stop")
-		stop();
-	else{
-		departure();
+	if (Serial.available()){
+		command = Serial.read();
+		if (command == 1){
+			departure();
+		}
+		else{
+			stop();
+		}
 	}
 }
-
 	
 	
 
